afa1149eed00c51e46d27897efd561f50a17b536
yzrobot <yz@ai.univ-paris8.fr>
2017-09-09 19:37:50 +0200

    update README.md

--- MORSE/mapping.sh
+++ MORSE/mapping.sh
@@ -9,12 +9,12 @@ then
 fi
 
 scenario=$(pwd)/$(echo $1 | cut -f 1 -d '.')
-sed -i 's?blender_file?'`echo $scenario`'?' singlerobot.py
+sed -i 's?blender_file?'$(echo $scenario)'?' singlerobot.py
 
 PARAMS=()
 PARAMS+=( --tab -e "bash -c 'roscore;exec bash'" )
-PARAMS+=( --tab -e "bash -c 'morse run -g 768x768 `pwd`/singlerobot.py;exec bash'" )
+PARAMS+=( --tab -e "bash -c 'morse run -g 768x768 $(pwd)/singlerobot.py;exec bash'" )
 PARAMS+=( --tab -e "bash -c 'sleep 3;roslaunch mapping.launch;exec bash'" )
-gnome-terminal --working-directory=`pwd` "${PARAMS[@]}"
+gnome-terminal --working-directory=$(pwd) "${PARAMS[@]}"
 
-sed -i 's?'`echo $scenario`'?blender_file?' singlerobot.py
+sed -i 's?'$(echo $scenario)'?blender_file?' singlerobot.py
--- MORSE/test.sh
+++ MORSE/test.sh
@@ -9,11 +9,11 @@ then
 fi
 
 scenario=$(pwd)/$(echo $1 | cut -f 1 -d '.')
-sed -i 's?blender_file?'`echo $scenario`'?' multirobot.py
+sed -i 's?blender_file?'$(echo $scenario)'?' multirobot.py
 
 PARAMS=()
 PARAMS+=( --tab -e "bash -c 'roscore;exec bash'" )
-PARAMS+=( --tab -e "bash -c 'morse run -g 640x640 `pwd`/multirobot.py;exec bash'" )
-gnome-terminal --working-directory=`pwd` "${PARAMS[@]}"
+PARAMS+=( --tab -e "bash -c 'morse run -g 640x640 $(pwd)/multirobot.py;exec bash'" )
+gnome-terminal --working-directory=$(pwd) "${PARAMS[@]}"
 
-sed -i 's?'`echo $scenario`'?blender_file?' multirobot.py
+sed -i 's?'$(echo $scenario)'?blender_file?' multirobot.py
